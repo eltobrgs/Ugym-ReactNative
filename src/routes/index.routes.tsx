04f5572e863afb3e3fac5c46aeee77f965c1f034
elto borges <eltobrgs@MacBook-Air-de-elto.local>
2025-01-19 13:55:59 -0300

    Renamed export directory to web-build for Vercel

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -53,10 +53,6 @@ export default function Routes() {
             <Stack.Screen
                 name="PreferencesScreen"
                 component={PreferencesScreen} />
-            
-
-            
-
         </Stack.Navigator>
     );
 }
